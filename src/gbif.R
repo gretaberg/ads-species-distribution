98b3029dbff14c2fdf947c2c41d5138df5dea609
gretaberg <gretaberg@lclark.edu>
2024-02-13 21:01:02 +0000

    data cleaned

--- src/gbif.R
+++ src/gbif.R
@@ -24,6 +24,19 @@ d <- occ_download_get('0012224-240202131308920',path='data/') %>%
   occ_download_import()
 write_csv(d,'data/rawData.csv')
 
+#cleaning
+filterData<-d %>%
+  filter(!is.na(decimalLatitude),!is.na(decimalLongitude))
+#above gets rid of n/a spots in long/lat 
+filterData<-filterData%>%
+  filter(countryCode %in% c('US','CA','MX'))
+#above keeps all data collect from US, CA, and MX
+filterData<-filterData%>%
+  filter(!basisOfRecord %in% c('FOSSIL_SPECIMEN','LIVING_SPECIMEN'))
+#above gets rid of fossil records and zoo records 
+filterData<-filterData%>%
+  cc_sea(lon='decimalLongitude',lat='decimalLatitude')
+#gets rid of records from the sea 
 
 
 
